15f1e2ac2bececc1c33f5392c5281d2e12200201
Bak David <david@webgurus.biz>
2023-03-22 16:05:11 +0200

    Added home, about, products pages with default and custom layout

---
+++ pages/products/[id].vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <p>Product details for {{ id }}</p>
+  </div>
+</template>
+
+<script setup>
+const { id } = useRoute().params
+definePageMeta({
+  layout: 'products',
+})
+</script>
+
+<style lang="scss" scoped></style>
